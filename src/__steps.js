b2a1de1b59ccdd528a8d3d13abed025791678276
Programming Hero <programming.hero1@gmail.com>
2022-10-06 23:19:20 -0500

    added _redirects for router deploy

--- src/__steps.js
+++ src/__steps.js
@@ -13,8 +13,8 @@ Basic Router Setup:
 /* 
 
 Context API
-1. declare a context 
-2. set provider with value
+1. declare a context using createContext
+2. set provider with value [single value, object, array]
 3. useContext in the place where you want to get the context
 
 */
